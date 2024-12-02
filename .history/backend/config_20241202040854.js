3df02bd018549f3072ff43c9fdcefddd7e26ed26
Colin Lee <clkrdesign@gmail.com>
2024-12-02 04:11:53 -0500

    Added port and mongouri

--- .history/backend/config_20241202040854.js
+++ .history/backend/config_20241202040854.js
@@ -1,4 +1,4 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT || 5555;
 
 export const mongoDBURL = 'mongodb+srv://root:root@alanapp.5ayrpxa.mongodb.net/review-collection?retryWrites=true&w=majority'
 
